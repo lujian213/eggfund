98bebdfe3315de14303f3508f4de8958f7e61561
lujian213 <lujian213@msn.com>
2025-03-02 16:23:26 +0800

    update sample test case

--- src/test/groovy/io/github/lujian213/eggfund/EggFundApplicationSpec.groovy
+++ src/test/groovy/io/github/lujian213/eggfund/EggFundApplicationSpec.groovy
@@ -1,10 +1,18 @@
 package io.github.lujian213.eggfund
 
+import io.github.lujian213.eggfund.config.ExecutorConfig
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContext
 import spock.lang.Specification
 
+@SpringBootTest(classes = ExecutorConfig)
 class EggFundApplicationSpec extends Specification {
+    @Autowired
+    ApplicationContext context
+
     def "test main"() {
         expect:
-        1 == 1
+        context.getBean("fundValueRetrieveExecutor")
     }
 }
