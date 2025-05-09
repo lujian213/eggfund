dea29556a3bb3ed330a991af074227c067260ef1
lujian213 <lujian213@msn.com>
2025-05-09 17:19:43 +0800

    Add server configuration to OpenAPI for API routing

--- src/main/java/io/github/lujian213/eggfund/config/OpenApiConfig.java
+++ src/main/java/io/github/lujian213/eggfund/config/OpenApiConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +15,7 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+            .addServersItem(new Server().url("/api"))
             .info(new Info().title("API Docs").version("1.0"))
             .addSecurityItem(new SecurityRequirement().addList("basicAuth"))
             .components(new Components()
