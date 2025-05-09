a3d5c941a1b28ebf36f5e9b08c497020bde1b152
lujian213 <lujian213@msn.com>
2025-05-09 17:46:09 +0800

    Remove unused server configuration from OpenAPI and add Nginx proxy for Swagger UI

--- src/main/java/io/github/lujian213/eggfund/config/OpenApiConfig.java
+++ src/main/java/io/github/lujian213/eggfund/config/OpenApiConfig.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,7 +14,6 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-            .addServersItem(new Server().url("/api"))
             .info(new Info().title("API Docs").version("1.0"))
             .addSecurityItem(new SecurityRequirement().addList("basicAuth"))
             .components(new Components()
